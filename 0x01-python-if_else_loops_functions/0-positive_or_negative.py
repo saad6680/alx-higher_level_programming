41c9c9d311fcde64e831d72bd54d2fb23e1f27bb
saad6680 <saadezz645@gmail.com>
2024-03-13 15:49:35 -0700

    tas1

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -4,5 +4,4 @@ import numpy as np
 
 number = random.randint(-10, 10)
 
-print(f"{number} is {'positive' if number > 0 else 'negative'
-if number < 0 else 'zero'}")
+print(f"{number} is {'positive' if number > 0 else 'negative'if number < 0 else 'zero'}")
