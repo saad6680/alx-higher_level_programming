9eeb28b79f7cb71ea1594c79fb4204dc1d38a96a
saad6680 <saadezz645@gmail.com>
2024-03-13 15:45:01 -0700

    tas1

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import random
+import numpy as np
+
+number = random.randint(-10, 10)
+
+print(f"{number} is {'positive' if number > 0 else 'negative' if number < 0 else 'zero'}")
